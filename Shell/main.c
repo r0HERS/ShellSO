6a2ce1d62159f90949cbfba026330e6024dcbf7d
unknown <re_rohers@hotmail.com>
2024-05-16 12:45:33 -0300

    so falte detalhes minimos

--- Shell/main.c
+++ Shell/main.c
@@ -11,11 +11,6 @@
 #include <pwd.h>
 #include <grp.h>
 
-
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
-
-
 #define LSH_RL_BUFFER 1024
 #define LSH_TOK_BUFFERSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
@@ -24,23 +19,16 @@
 char *path_dirs[MAX_PATH_DIRS];
 int num_path_dirs = 0;
 
-
 int cd(char **commands);
 int help(char **commands);
 int shell_exit(char **commands);
 int shell_path(char **commands);
-int ls(char **commands);
-int cat(char **commands);
-void cat_redirect(char **commands);
-
 
 char *builtin_str[] = {
     "cd",
     "help",
     "exit",
     "path"
-
-
 };
 
 int (*builtin_fun[])(char **) = {
@@ -48,8 +36,6 @@ int (*builtin_fun[])(char **) = {
     &help,
     &shell_exit,
     &shell_path
-
-
 };
 
 int num_builtins() {
@@ -65,7 +51,7 @@ void remove_newline(char *str) {
 
 int cd(char **commands) {
     if (commands[1] == NULL) {
-        fprintf(stderr, "\nNo argument, expected : \"cd\"\n");
+        fprintf(stderr, "\nSem argumento, esperado: \"cd <folder> \n");
     } else {
         if (chdir(commands[1]) != 0) {
             int found = 0;
@@ -94,13 +80,14 @@ int help(char **commands) {
 }
 
 int shell_exit(char **commands) {
+    fflush(stdin);
+    fflush(stdout);
     return 0;
 }
 
 int shell_path(char **commands) {
-
     if (commands[1] == NULL) {
-        printf("Current path directories:\n");
+        printf("Paths atuais:\n");
         for (int i = 0; i < num_path_dirs; i++) {
             printf("    %s\n", path_dirs[i]);
         }
@@ -109,7 +96,7 @@ int shell_path(char **commands) {
             if (num_path_dirs < MAX_PATH_DIRS - 1) {
                 path_dirs[num_path_dirs++] = strdup(commands[i]);
             } else {
-                fprintf(stderr, "Max path directories exceeded\n");
+                fprintf(stderr, "Maximo numero de paths excedido\n");
                 break;
             }
         }
@@ -117,37 +104,32 @@ int shell_path(char **commands) {
     return 1;
 }
 
-/*char *find_executable(const char *commands, int start) {
+char *find_executable(const char *executable) {
     char *full_path = NULL;
 
-    // Percorre todos os diretórios em path_dirs
     for (int i = 0; i < num_path_dirs; i++) {
-        // Tamanho necessário para alocar espaço para o caminho completo
-        size_t path_len = strlen(path_dirs[i]) + strlen(commands(start)) + 2;  // +2 para '/' e '\0'
+        size_t path_len = strlen(path_dirs[i]) + strlen(executable) + 2;  // +2 for '/' and '\0'
         char *path_exec = (char *)malloc(path_len);
 
         if (path_exec == NULL) {
-            perror("Memory allocation error");
+            perror("Alocaçao de  memoria ERROR");
             continue;
         }
 
-        // Constrói o caminho completo para o executável
         snprintf(path_exec, path_len, "%s/%s", path_dirs[i], executable);
 
-        // Verifica se o executável existe nesse caminho
+        printf("\nBuscando em: %s\n\n", path_exec);
+
         if (access(path_exec, X_OK) == 0) {
-            // Executável encontrado, retorna o caminho completo
             full_path = path_exec;
             break;
         }
 
-        // Liberar a memória alocada para o caminho temporário
         free(path_exec);
     }
 
     return full_path;
-}*/
-
+}
 
 void *read_input() {
     int BUFFER_SIZE = LSH_RL_BUFFER;
@@ -156,8 +138,8 @@ void *read_input() {
     int c;
 
     if (!buffer) {
-        fprintf(stderr, "Buffer allocation space ERROR\n");
-        exit(EXIT_FAILURE);
+        fprintf(stderr, "Alocaçao do buffer ERROR\n");
+        return NULL;
     }
 
     while (1) {
@@ -172,11 +154,11 @@ void *read_input() {
         pos++;
 
         if (pos >= BUFFER_SIZE) {
-            BUFFER_SIZE = BUFFER_SIZE + LSH_RL_BUFFER;
+            BUFFER_SIZE += LSH_RL_BUFFER;
             buffer = realloc(buffer, BUFFER_SIZE);
             if (!buffer) {
-                fprintf(stderr, "Buffer reallocation space ERROR\n");
-                exit(EXIT_FAILURE);
+                fprintf(stderr, "Realocaçao de buffer ERROR\n");
+                return NULL;
             }
         }
     }
@@ -189,8 +171,8 @@ char **split_input(char *input) {
     char *token;
 
     if (!tokens) {
-        fprintf(stderr, "Tokens allocation space ERROR\n");
-        exit(EXIT_FAILURE);
+        fprintf(stderr, "Tokens alocaçao ERROR\n");
+        return NULL;
     }
 
     token = strtok(input, LSH_TOK_DELIM);
@@ -203,8 +185,8 @@ char **split_input(char *input) {
             BUFFER_SIZE += LSH_TOK_BUFFERSIZE;
             tokens = realloc(tokens, BUFFER_SIZE * sizeof(char *));
             if (!tokens) {
-                fprintf(stderr, "Tokens reallocation space ERROR\n");
-                exit(EXIT_FAILURE);
+                fprintf(stderr, "Tokens realocaçao ERROR\n");
+                return NULL;
             }
         }
         token = strtok(NULL, LSH_TOK_DELIM);
@@ -213,49 +195,26 @@ char **split_input(char *input) {
     return tokens;
 }
 
-int launch_comand(char **commands,int start) {
-    pid_t pid, wpid;
-    int status;
+int launch_command(char **commands, int start) {
+    char *executable = find_executable(commands[start]);
 
-    pid = fork();
-
-    if (pid == 0) {
-
-        if(strcmp(commands[start],"ls") == 0 ){
-
-            if (execl("./ls","ls",commands[start+1],commands[start+2],NULL) == -1) {
-                perror("Execvp execution ERROR");
-                exit(EXIT_FAILURE);
-            }
-
-        }
-
-        if(strcmp(commands[start],"cat") == 0 ){
-
-            if (execl("./cat","cat",commands[start+1],commands[start+2],commands[start+3],NULL) == -1) {
-                perror("Execvp execution ERROR");
-                exit(EXIT_FAILURE);
-            }
-
-        }
-
-    } else if (pid < 0) {
-        perror("Fork failed");
-    } else {
-        do {
-            wpid = waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    if (executable == NULL) {
+        fprintf(stderr, "Executavel não encontrado\n");
+        return 1;
     }
 
+    if (execl(executable, commands[start], commands[start + 1], commands[start + 2], commands[start + 3], NULL) == -1) {
+        perror("Execl exucuçao ERROR");
+        return 1;
+    }
+
+    free(executable);
     return 1;
 }
 
-int exec_comand(char **commands) {
-
+int exec_command(char **commands) {
     int parallel = 0;
     int i = 0;
-    int start = 0;
-    int end = 0;
 
     if (commands[0] == NULL) {
         return 1;
@@ -269,39 +228,65 @@ int exec_comand(char **commands) {
         i++;
     }
 
-    if (parallel==1) {
-        while (commands[end] != NULL) {
-            if (strcmp(commands[end], "&") == 0) {
-                commands[end] = NULL;
+    if (parallel == 1) {
+        int start = 0;
+        for (int j = 0; commands[j] != NULL; j++) {
+            if (strcmp(commands[j], "&") == 0) {
+                commands[j] = NULL;
                 pid_t pid = fork();
 
                 if (pid == 0) {
-                    for (int i = 0; i < num_builtins(); i++) {
-                        if (strcmp(commands[start], builtin_str[i]) == 0) {
-                            return (*builtin_fun[i])(commands);
-                            }
+                    for (int k = 0; k < num_builtins(); k++) {
+                        if (strcmp(commands[start], builtin_str[k]) == 0) {
+                            exit((*builtin_fun[k])(commands + start));
+                        }
                     }
-
-                    launch_comand(commands,start);
-                    exit(EXIT_FAILURE);
+                    launch_command(commands, start);
+                    return 1;
                 } else if (pid < 0) {
                     perror("fork ERROR");
                 }
-                start = end + 1;
+                start = j + 1;
             }
-            end++;
-        }
         }
 
-    for (int i = 0; i < num_builtins(); i++) {
-        if (strcmp(commands[0], builtin_str[i]) == 0) {
-            return (*builtin_fun[i])(commands);
+        if (commands[start] != NULL && commands[start][0] != '\0') {
+            pid_t pid = fork();
+
+            if (pid == 0) {
+                for (int k = 0; k < num_builtins(); k++) {
+                    if (strcmp(commands[start], builtin_str[k]) == 0) {
+                        return (*builtin_fun[i])(commands + start);
+                    }
+                }
+                launch_command(commands, start);
+                return 1;
+            } else if (pid < 0) {
+                perror("fork ERROR");
+            }
+        }
+
+        while (wait(NULL) > 0);
+    } else {
+        for (int i = 0; i < num_builtins(); i++) {
+            if (strcmp(commands[0], builtin_str[i]) == 0) {
+                return (*builtin_fun[i])(commands);
+            }
+        }
+
+        pid_t pid = fork();
+        if (pid == 0) {
+            launch_command(commands, 0);
+            return 1;
+        } else if (pid < 0) {
+            perror("Fork ERROR");
+        } else {
+            wait(NULL);
         }
     }
 
-    return launch_comand(commands,start);
+    return 1;
 }
-
 
 void text() {
     char cwd[1024];
@@ -319,7 +304,7 @@ int main(int argc, char **argv) {
         text();
         input = read_input();
         commands = split_input(input);
-        status = exec_comand(commands);
+        status = exec_command(commands);
 
         free(input);
         free(commands);
